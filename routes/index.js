0b9d09ac80ca830f3ac07f5cc73319a5259cfec3
Marisa Kanemoto <marisa.kanemoto@gmail.com>
2016-01-28 19:47:06 +0000

    pineapple

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,48 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+  	'projects': [
+  		{
+	  		'name': 'The Freshman Experience',
+	  		'image': 'comingsoon.jpg',
+	  		'id': 'project1'
+  		},
+  		{
+  			'name': 'Needfinding',
+  			'image': 'comingsoon.jpg',
+  			'id': 'project2'
+  		},
+  		{
+  			'name': 'Prototyping',
+  			'image': 'comingsoon.jpg',
+  			'id': 'project3'
+  		},
+  		{ 
+  			'name': 'Heuristic Evaluation',
+        	'image': 'comingsoon.jpg',
+       		'id': 'project4'
+		},
+		{ 
+			'name': 'Visualization',
+			'image': 'comingsoon.jpg',
+			'id': 'project5'
+		},
+		{
+			'name': 'Social Design',
+			'image': 'comingsoon.jpg',
+			'id': 'project6'
+		},
+		{
+			'name': 'Gestural Interaction',
+			'image': 'comingsoon.jpg',
+			'id': 'project7'
+		},
+		{
+			'name': 'Design Tools',
+			'image': 'comingsoon.jpg',
+			'id': 'project8'
+		}
+  	]
+  });
 };
